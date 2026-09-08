82b7690ef0a05734b76896507723922c11dbdec2
kowsikalluri <20012ee005@gmail.com>
2026-09-08 19:35:50 +0530

    Add RTC configuration with ICE servers to media stream

--- app.py
+++ app.py
@@ -445,6 +445,15 @@ webrtc_streamer(
         "video": True,
         "audio": False,
     },
+    rtc_configuration={
+        "iceServers": [
+            {
+                "urls": [
+                    "stun:stun.l.google.com:19302"
+                ]
+            }
+        ]
+    },
 
     async_processing=True,
 )
